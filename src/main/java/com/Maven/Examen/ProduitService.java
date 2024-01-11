6d443c64ebb33392560f7f8d1fcbadb5e6beba2d
SaraELASRI11 <sarrita.elas@gmail.com>
2024-01-11 14:59:47 +0100

    ajout de la fonction Read Produit

--- src/main/java/com/Maven/Examen/ProduitService.java
+++ src/main/java/com/Maven/Examen/ProduitService.java
@@ -2,4 +2,12 @@ package com.Maven.Examen;
 
 public class ProduitService {
 
+	  public Produit readProduit(Long id) {
+	        for (Produit p : produits) {
+	            if (p.getId().equals(id)) {
+	                return p;
+	            }
+	        }
+	        return null;
+	    }
 }
